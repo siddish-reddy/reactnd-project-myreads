98ab0394141ab757a39370cd2bc3dd6a6a489831
siddish reddy <Siddish.ibv@gmail.com>
2018-11-11 00:40:59 +0530

    Removed error while results are not found

--- src/Search.js
+++ src/Search.js
@@ -28,22 +28,22 @@ class Search extends React.Component{
         return (
             <div className="search-books">
 
-            <div className="search-books-bar">
-              <Link to = '/' className="close-search" >Close</Link>
-              <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author" value = {this.state.searchQuery} onChange = {this.handleInputChange}/>
-              </div>
-            </div>
+                <div className="search-books-bar">
+                    <Link to = '/' className="close-search" >Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <input type="text" placeholder="Search by title or author" value = {this.state.searchQuery} onChange = {this.handleInputChange}/>
+                    </div>
+                </div>
 
-            <div className="search-books-results">
-              {this.state.results && 
-                <ol className="books-grid">
-                    {this.state.results.map(book=>(
-                        <li><Book book = {book} handleShelfChange={this.props.handleShelfChange}/></li>
-                    ))}
-                </ol>
-              }
-            </div>
+                <div className="search-books-results">
+                    {this.state.results && 
+                        <ol className="books-grid">
+                            {this.state.results.map  && this.state.results.map (book=>(
+                                <li key={book.id}><Book book = {book} handleShelfChange={this.props.handleShelfChange}/></li>
+                            ))}
+                        </ol>
+                    }
+                </div>
           </div>
         )
     }
