4e4ad651bfc275fb56166e1fd88ee779ecf03a6a
siddish reddy <Siddish.ibv@gmail.com>
2018-11-11 11:38:08 +0530

    fixed thumbnail not found bug

--- src/Book.js
+++ src/Book.js
@@ -4,10 +4,12 @@ class Book extends React.Component{
     render() {
         if( this.props.book){
             const book = this.props.book
+            console.log(book)
+            const thumbnail = book.imageLinks ? book.imageLinks.smallThumbnail || book.imageLinks.thumbnail : null
             return (
                 <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${thumbnail})` }}></div>
                         <div className="book-shelf-changer">
                             <select onChange={e => { this.props.handleShelfChange(book, e.target.value)}} value={book.shelf} >
                                 <option value="move" disabled>Move to...</option>
