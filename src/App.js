22d9b1113434a492902cbd7fe21db4141dd8d920
siddish-reddy <siddish.ibv@gmail.com>
2019-07-18 10:54:20 +0530

    Sweet alert added instead of normal alert

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import {update, getAll} from './BooksAPI'
 import MyReads from './MyReads'
 import Search from './Search'
 import NoMatch from './404'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 import './App.css'
+
+const MySwal = withReactContent(Swal);
 
 class App extends React.Component {
     state ={
@@ -16,7 +20,7 @@ class App extends React.Component {
             this.setState(s=>({
                 books : s.books.filter(b=>b.title!==book.title).concat(book)
             }))
-            alert(book.title+ ' is added succesfully into "'+ shelf+'" shelf');
+            MySwal.fire(<p style={{fontSize: 16}}>{book.title}  is added succesfully into {shelf} shelf</p>);
         })
     }
     componentDidMount(){
